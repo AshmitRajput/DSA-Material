c3eb7a9800a1ae7c9aa63ad7dffbdb854c002b38
AshmitRajput <ashmitrajput1007@gmail.com>
2025-07-15 22:24:31 +0530

    Lecture-3 (About basic use of operators and cout/cin)

---
+++ L1.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+using namespace std;
+int main (){
+
+/*                                  program 1 to find maximum of two numbers
+
+int a,b;
+
+cout<<"Enter the value of a \n";
+cin>>a;
+cout<<"Enter the value of b \n";
+cin>>b;
+
+if(a>b){
+    cout<<"a is greater than b \n";
+} 
+if(b>a){
+    cout<<"b is greater than a \n";
+}
+if(a==b){
+    cout<<"a is equal to b \n";
+}
+*/
+
+
+/*                                   program2 to find positive and negative no.
+int a;
+cout<<"enter the value of a \n";
+cin>>a;
+
+if(a>0){
+    cout<<"a is a positive no.";
+}
+else{
+    if(a<0){
+        cout<<"a is a negative no. \n";
+    }
+    else{
+        cout<<"a is zero";
+    }
+}
+*/
+
+/*                       if you use else if, it is read by system only if the previous if 
+                         statement was false. It is not checked individually. Use several if 
+                         statements if you want them all to be checked. 
+
+int a=20;
+if(a>15){
+    cout<<"yes";
+}
+else if(a==20){
+    cout<<"20";
+}
+else{
+    cout<<"no";
+}
+cout<<a;
+*/
+
+                    /*  use of logical operation &&, can't use 5<a<10, doesn't work.
+int a;
+cout<<"enter any number \n";
+cin>> a;
+if(5<a&&a<10){
+    cout<<"yes \n";
+} 
+else{
+    cout<<"nooo";
+}
+*/
+
+
+
+
+}
