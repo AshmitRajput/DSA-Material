3dc5960358ba15028cd50202ce2dd4c64819f851
AshmitRajput <ashmitrajput1007@gmail.com>
2025-07-18 20:37:12 +0530

    L4 - Decimal & Binary

---
+++ L4.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main(){
+                   
+                   /*DECIMAL TO BINARY CONVERTOR*/
+    /*
+    int n,ans=0,i=0;
+    cout<<"Enter a decimal number:";
+    cin>>n;
+
+    while (n !=0)
+    {
+        int bit = n&1;
+        ans = (bit* pow(10,i)) + ans;
+        n= n>>1;
+        i++;
+    }
+    
+    cout<<ans+1;
+        */ /*COUT M ANS + 1 KARNE PE SAHI AA RAHA H NOT BY JUST ANS, DON'T KNOW THE ERROR*/
+
+
+            /*BINARY INPUT TO DECIMAL CONVERSION*/
+
+            
+    /* int n,i=0,ans=0,digit;
+
+    cout<<"Enter a binary number:";
+    cin>>n;
+
+    while (n != 0)
+    {
+        digit=n%10;
+        n=n/10;
+        
+        if (digit==1)
+        {
+            ans = digit*pow(2,i) + ans;
+    
+        }
+     i++;
+    }
+    cout<<ans;     
+
+return 0; */
+
+}
