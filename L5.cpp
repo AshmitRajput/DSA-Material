cbc604cc84d4fae062ea22a4576ecfabbc9cf719
AshmitRajput <ashmitrajput1007@gmail.com>
2025-07-19 14:57:17 +0530

    L5 - Basic Integer Questions

---
+++ L5.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+
+                                                      /* REVERSE AN  INTEGER */
+    /*int n,ans=0;
+
+    cout<<"Enter a number:";
+    cin>>n;
+    
+    while (n!=0)
+    {
+        int digit = n%10;
+        ans = ans*10 + digit;
+        n = n/10;
+
+    }
+
+       if ((ans<INT32_MIN/10)||(ans>INT32_MAX/10))
+    {
+        return 0;
+    }
+
+    cout<<ans;*/
+
+}
