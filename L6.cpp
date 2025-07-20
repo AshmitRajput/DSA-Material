2e0f80dbfce234e1492b480b75d2e37632782db2
AshmitRajput <ashmitrajput1007@gmail.com>
2025-07-20 22:08:01 +0530

    L6-Understanding Switch-Case statement

---
+++ L6.cpp
@@ -0,0 +1,86 @@
+/*USING A SWITCH-CASE STATEMENT AND UNDERSTANDING WHY CONTINUE STATEMENT DOESN'T 
+  WORK IN A SWITCH CASE STATEMENT */
+
+  #include<iostream>
+  using namespace std;
+  
+                  //AP FUNCTION BODY.
+ /* 
+  int AP(int n)
+{
+  int ap=3*n+7;
+  return ap;
+}*/
+
+             //SETBITS FUNCTION BODY.
+/*
+
+int bitsa(int a)
+{
+  int count = 0;
+  while(a!=0)
+  {
+    if(a&1)
+    {
+      count++;
+    }
+    a=a>>1;
+  }
+  return count; 
+}
+
+int bitsb(int b)
+{
+  int count = 0;
+  while(b!=0)
+  {
+    if(b&1)
+    {
+      count++;
+    }
+    b=b>>1;
+  }
+  return count; 
+}
+*/
+
+                         //FIBONACCI SERIES FUNCTION BODY
+/*
+ int fibonacci(int n)
+{
+    int a = 0;
+    int b = 1;
+    for (int i = 3; i < n; i++)
+    {
+        int next = a + b;
+        a = b;
+        b = next;
+    } 
+      return b;
+}
+*/
+
+  int main(){
+
+                                         //EXAMPLE
+   /* int n;
+    cout<<"Enter a number from 1 to 3 :\n";
+    cin>>n;
+
+    switch (n)
+    {
+    case 1:
+        cout<<"padh le bhai\n";
+        break;
+    case 2:
+        cout<<"jaake bartan saaf kar\n";
+        break;
+    case 3:
+        cout<<"jhadu maar room m \n";
+        break;    
+    
+    default:
+        cout<<"kuch bhi bhai\n";
+    }
+    */
+   }
