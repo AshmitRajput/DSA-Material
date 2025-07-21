9f737e6921ab1ca7efe1a41f2df5621e66e02979
AshmitRajput <ashmitrajput1007@gmail.com>
2025-07-21 20:28:13 +0530

    L7-Basic Functions to print/reverse/get maximum/get minimun in an array

---
+++ L7.cpp
@@ -0,0 +1,71 @@
+ #include <iostream>
+ using namespace std;
+
+                                               //PRINTING ALL THE ELEMENTS OF AN ARRAY.
+ /*
+ void printArray(int arr[], int n) {
+    
+    for(int i=0; i<n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}*/
+
+                                               //GET MINIMUM IN AN ARRAY FUNCTION
+ /*int getmin(int num[], int n){
+    int mini = INT32_MAX;
+
+    for (int i = 0; i <= n; i++)
+    {
+        mini = min(mini,num[i]);
+
+        //if (num[i]<mini)
+        //{
+       //     mini = num[i];
+       // }
+        
+    }
+    return mini;
+
+ }*/
+
+                                            //GET MAXIMUM VALUE IN AN ARRAY FUNCTION
+  /*int getmax(int num[], int n){
+    int maxi = INT32_MIN;
+    for (int i = 0; i <= n; i++)
+    {
+        maxi = max(maxi,num[i]);
+         //if (num[i]>maxi)
+        //{
+       //     mini = num[i];
+       // }
+    }
+    return maxi;
+ }*/
+
+                                                       //LINEAR SEARCH FUNCTION.
+/* 
+bool search(int arr[], int size, int key) {
+
+    for( int i = 0; i<size; i++ ) {
+
+        if( arr[i] == key) {
+            return 1;
+        }
+
+    }
+    return 0;
+}*/
+
+                                                       //REVERSE AN ARRAY FUCTION.
+/*void reverse(int arr[], int n) {
+
+    int start = 0;
+    int end = n-1;
+
+    while(start<=end) {
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+}*/
