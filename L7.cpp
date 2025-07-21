c147c29d5618d120791b51c6a0ef3334bc4a3605
AshmitRajput <ashmitrajput1007@gmail.com>
2025-07-21 23:14:17 +0530

    L7-Different Operations with Arrays

--- L7.cpp
+++ L7.cpp
@@ -69,3 +69,90 @@ bool search(int arr[], int size, int key) {
         end--;
     }
 }*/
+
+int main(){
+
+                                  //INITIALIZE AN ARRAY WITH VALUE 2 USING FILL_N COMMAND
+   /*int ar[10];
+    fill_n(ar,10,2);          //fill_n(data type,number of values, value to fill with)
+   for (int i = 0; i <= 10; i++)
+    {
+      cout << ar[i] << endl;
+    }*/
+
+                      //TAKE INPUT IN AN ARRAY AND FIND MAXIMUM AND MINIMUM VALUE IN IT
+    /*
+    int size;
+    cin>>size;
+
+    int num[100];
+    for (int i = 0; i < size; i++)
+    {
+        cin>>num[i];
+    }
+    
+    int mini = getmin(num[100],size);
+    cout<<"minimum value in array is:\n"<<mini;
+
+    int maxi = getmax(num[100],size);
+    cout<<"maximum value in array is:\n"<<maxi;
+    */
+
+                                        //FINIDNG SUM OF ALL ELEMENTS OF AN ARRAY
+    /*
+
+    int size;
+    cout<<"Enter size of Array:\n";
+    cin>>size;
+
+    int num[100];
+    for (int i = 0; i < size; i++)
+    {
+        cout<<"Enter value of element "<<(i+1);
+        cin>>num[i];
+    }
+
+    int sum = 0;
+    for (int i = 0; i < size; i++)
+    {
+        sum += num[i];
+    }
+    cout<<"The sum of all elements in the array is:\n"<<sum;
+
+    */
+
+
+                             //CODE FOR LINEAR SEARCH IN AN ARRAY TO FIND ANY ELEMENT
+   /* int arr[10] = { 5, 7, -2, 10, 22, -2, 0, 5, 22, 1};
+
+    cout <<" Enter the element to search for " << endl; 
+    int key;
+    cin >> key;
+
+    bool found = search(arr, 10, key);
+
+    if( found ) {
+        cout <<" Key is present "<< endl;
+    }
+    else{
+        cout <<" Key is absent " << endl;
+    }
+
+
+    return 0;
+    */
+
+                                  //REVERSING THE ELEMENTS IN AN ARRAY AND PRINTING IT
+   /*
+    int arr[6] = {1,4,0,5,-2,15};
+    int brr[5] = {2,6,3,9,4};
+
+    reverse(arr, 6);
+    reverse(brr, 5);
+
+    printArray(arr, 6);
+    printArray(brr, 5);
+
+    */
+
+   }
