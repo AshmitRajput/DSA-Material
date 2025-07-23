3003d87d3d445f5d580ac8fb87030a3e6ad3dafd
AshmitRajput <ashmitrajput1007@gmail.com>
2025-07-23 15:05:30 +0530

    L9-About Time and  Space complexity

---
+++ L9.cpp
@@ -0,0 +1,17 @@
+/* 
+                                TIME AND SPACE COMPLEXITY
+  constraints like 1<n<10^6
+  or 1<n<1000
+
+  for them to avoid TLE
+        <[10...11]   - O(n!).O(n^6)  
+        <[15...18]   - O(2^n * n^2)
+        <[100]       - O(n^4)
+        <[400]       - O(n^3)
+        <[2000]      - O(n^2 * log n)
+        <[10^4]      - O(n^2)
+        <[10^6]      - O(nlog n)
+        <[10^8]      - O(n), O(log n)
+
+
+*/
