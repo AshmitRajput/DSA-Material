210bc2a777b351b0084b6d7469f79cbe9ef3692d
AshmitRajput <ashmitrajput1007@gmail.com>
2025-07-23 15:48:03 +0530

    L9-About Time and  Space complexity

--- L9.cpp
+++ L9.cpp
@@ -3,6 +3,28 @@
   constraints like 1<n<10^6
   or 1<n<1000
 
+
+  Time Complexity - Measures how the runtime of an algorithm grows relative
+   to the input size (usually denoted as n). 
+   It captures the number of steps (operations) your program will 
+   execute as input increases.
+
+  Space Complexity: Measures how much extra memory an algorithm 
+  requires as input size increases.
+
+  Always optimize for the worst-case in competitive 
+  programming unless problem states otherwise.
+
+  Before coding, estimate allowed operations 
+  (e.g., 1 second ≈ 100 million simple operations).
+
+  If your initial solution is too slow, look for a more 
+  efficient algorithm (typically, reducing a nested loop or using 
+  better data structures).
+
+  Use libraries for standard complex operations when 
+  allowed (like built-in sorts).
+
   for them to avoid TLE
         <[10...11]   - O(n!).O(n^6)  
         <[15...18]   - O(2^n * n^2)
